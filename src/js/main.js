7f51b6e7773787eb1455ab668c508f34fd950f01
Mateusz <stefvnowicx@gmail.com>
2025-01-21 21:02:56 +0100

    nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
-const test = (params) => {
-    console.log('test');
-   //
-};
+const burgerBtn = document.querySelector('.burger-btn')
+const mobileNav = document.querySelector('.nav__mobile')
+
+const handleNav = () =>{
+    mobileNav.classList.toggle('nav__mobile--active')
+    burgerBtn.classList.toggle('burger-btn--active')
+}
+
+burgerBtn.addEventListener('click', handleNav)

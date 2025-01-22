326959173591c876533df6e1aedf0e63aa671062
Mateusz <stefvnowicx@gmail.com>
2025-01-22 13:54:58 +0100

    do poprawny text i img, zeby byl bardziej responsywny, nie sztywny

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,15 @@
 const burgerBtn = document.querySelector('.burger-btn')
 const mobileNav = document.querySelector('.nav__mobile')
 
+const allMobileLinks = document.querySelectorAll('.nav__mobile-link')
+
 const handleNav = () =>{
     mobileNav.classList.toggle('nav__mobile--active')
     burgerBtn.classList.toggle('burger-btn--active')
 }
+
+allMobileLinks.forEach(link => {
+    link.addEventListener('click', handleNav)
+})
 
 burgerBtn.addEventListener('click', handleNav)
